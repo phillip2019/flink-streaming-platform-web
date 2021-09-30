6c476aad84e45a9bfb9ad712c32e6be727ee90db
xiaowei.song <songxiaowei@chinagoods.com>
2021-09-30 15:04:38 +0800

    #Updates#调整jvm参数位置

--- flink-streaming-web-common/src/main/java/com/flink/streaming/web/common/util/CommandUtil.java
+++ flink-streaming-web-common/src/main/java/com/flink/streaming/web/common/util/CommandUtil.java
@@ -83,9 +83,9 @@ public class CommandUtil {
         }
         command.append(jobRunParamDTO.getFlinkRunParam()).append(" ");
         command.append(" -ynm ").append(JobConfigDTO.buildRunName(jobConfigDTO.getJobName())).append(" ");
-        command.append(" -yd -m yarn-cluster ").append(" ");
         // 设置uft8编码，避免文件乱码
         command.append(" -yD env.java.opts=\"-Djava.net.preferIPv4Stack=true -Dfile.encoding=utf-8 -Duser.language=zh\" ");
+        command.append(" -yd -m yarn-cluster ").append(" ");
 
         if (StringUtils.isNotEmpty(jobConfigDTO.getExtJarPath())) {
             String[] urls = jobConfigDTO.getExtJarPath().split(SystemConstant.LINE_FEED);
